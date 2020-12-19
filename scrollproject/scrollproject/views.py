0275dd0826429dfed07bd5411a5d5c78f7754b7f
vkfkd1107 <58639259+vkfkd1107@users.noreply.github.com>
2020-12-19 19:17:03 +0900

    pagination

--- scrollproject/scrollproject/views.py
+++ scrollproject/scrollproject/views.py
@@ -1,12 +1,35 @@
 from django.shortcuts import render
-from django.views.decorators.http import require_GET, require_POST
+from django.core.paginator import Paginator
+from django.http import Http404
+from django.views.generic import ListView
+
+# def home(request):
+
+#     numbers=range(0,1000)
+#     print(numbers)
+
+#     paginator=Paginator(numbers,50)
+#     page_num=int(request.GET.get("page",1))
+    
+#     if page_num > paginator.num_pages:
+#         raise Http404
+    
+#     numbers=paginator.page(page_num)    
+
+#     context={
+#         'numbers':numbers
+#     }
+#     return render(request,'home.html', context)
 
 def home(request):
-    numbers=[]
-    for i in range(0,1000):
-        numbers.append(i)
-    print(numbers)
+    numbers=range(0,1000)
+    paginator=Paginator(numbers,25)
+
+    page_num=request.GET.get('page')
+    page_obj=paginator.get_page(page_num)
+
     context={
-        'numbers':numbers
+        'page_obj':page_obj
     }
-    return render(request,'home.html')
+
+    return render(request,'home.html',context)
