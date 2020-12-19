586d8232f12f5ebdf42eb52a5c75236c9fba6b81
vkfkd1107 <58639259+vkfkd1107@users.noreply.github.com>
2020-12-19 16:56:34 +0900

    add script in html_file, add context in views.py

--- scrollproject/scrollproject/views.py
+++ scrollproject/scrollproject/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.decorators.http import require_GET, require_POST
 
 def home(request):
+    numbers=[]
+    for i in range(0,1000):
+        numbers.append(i)
+    print(numbers)
+    context={
+        'numbers':numbers
+    }
     return render(request,'home.html')
